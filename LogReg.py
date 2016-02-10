925ec087d1e9fa1765b3455620bdfe180434549b
Thomas Gui <tgui@ucsd.edu>
2016-02-10 01:18:32 -0800

    changed script to train on top 20 labels

--- LogReg.py
+++ LogReg.py
@@ -9,8 +9,8 @@ dataset = raw_input()
 print("Enter labels:")
 labelset = raw_input()
 
-print("Enter label to train on (integer between 0 to 1000):")
-label = int(raw_input())
+#print("Enter label to train on (integer between 0 to 1000):")
+#label = int(raw_input())
 
 print("Enter validation set:")
 validSet = raw_input()
@@ -18,30 +18,41 @@ validSet = raw_input()
 print("Enter validation label set:")
 validLabelSet = raw_input()
 
-logreg = LogisticRegression()
-
 data = pickle.load(open('pickles/'+ dataset, 'rb'))
 target = pickle.load(open('pickles/'+ labelset, 'rb'))
 valid = pickle.load(open('pickles/'+ validSet, 'rb'))
 validLabels = pickle.load(open('pickles/'+ validLabelSet, 'rb'))
 
-validLabels = validLabels[:, label:label+1]
-validLabels = np.asarray(validLabels.todense()).ravel()
-target = target[:, label:label+1]
-target = np.asarray(target.todense()).ravel()
+output = "test\n"
 
-logreg.fit(data, target)
-result = logreg.predict(valid)
+for j in range(0, 20):
+    logreg = LogisticRegression()
 
-count = 0
-length = len(validLabels)
-for i in range(0, length):
-    if(result[i] != validLabels[i]):
-        count += 1
+    validLabels1 = validLabels[:, j:j+1]
+    validLabels1 = np.asarray(validLabels1.todense()).ravel()
+    target1 = target[:, j:j+1]
+    target1 = np.asarray(target1.todense()).ravel()
 
-ratio = count / length
+    logreg.fit(data, target1)
+    result = logreg.predict(valid)
 
-print("%d errors in %d examples. Overall error is %d") % (count, length, ratio)
+    count = 0
+    length = len(validLabels1)
+    for i in range(0, length):
+        if(result[i] != validLabels1[i]):
+            count += 1
 
-with open('skLogReg.pkl', 'wb') as fp:
-    pickle.dump(logreg, fp, -1)
+    ratio = count / length
+
+    output += "%d errors in %d examples. Overall error is %d for label %d" % (count, length, ratio, j)
+
+    print("%d errors in %d examples. Overall error is %d for label %d") % (count, length, ratio, j)
+
+    filename = 'skLogRegLabel%d' % (j)
+
+    with open(filename, 'wb') as fp:
+        pickle.dump(logreg, fp, -1)
+
+with open('20labelstest.txt', 'wb') as file1:
+    file1.write(output)
+    file1.close()
