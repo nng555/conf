1dcdc6c829f80633ab9631c9bb2b8dcdbb3dcc5f
Nathan Ng <n.ng555@gmail.com>
2017-10-24 13:45:57 -0700

    deleted files

--- examine.py
+++
@@ -1,9 +0,0 @@
-with open("2Mdump.json", 'rb') as fh:
-   offs = -200
-   while True:
-      fh.seek(offs, 2)
-      lines = fh.readlines()
-      if len(lines)>3:
-         print lines[-2]
-         break
-      offs *= 2
--- load.py
+++
@@ -1,9 +0,0 @@
-import ijson
-def load_json(filename):
-   with open(filename) as fd:
-      parser = ijson.parse(fd)
-      for prefix, event, value in parser:
-         print prefix, value
-
-if __name__ == "__main__":
-   load_json('allMeSH.json')
